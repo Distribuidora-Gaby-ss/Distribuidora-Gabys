15fe6a75ac0b145a48d6f1651dcdd6e2de323236
Esteban Peña <120404832+Estebxnnnn@users.noreply.github.com>
2025-11-27 00:21:23 +0000

    HU-13-Filtrar-Productos

--- app.py
+++ app.py
@@ -75,7 +75,13 @@ def productos():
 
     query = request.args.get("q", "").strip().lower()
     tipo_filtro = request.args.get("tipo", "").strip().lower()
-    
+    if query or tipo_filtro:
+        productos = [
+            p for p in productos
+            if (not query or query in p["nombre"].lower() or query in p["codigo"].lower())
+            and (not tipo_filtro or p["tipo"].lower() == tipo_filtro)
+        ]
+
 
     return render_template("productos.html", productos=productos)
 
